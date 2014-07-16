44296207ec5660563771c924bfaf706cb275f4de
wuzihua <371219521@qq.com>
2014-07-16 08:45:06 +0800

    每日进度报告 By 吴梓华 7.16

--- 3.开发阶段/第一次迭代/android前端/Service/src/client/ui/SendHelpMsgActivity.java
+++ 3.开发阶段/第一次迭代/android前端/Service/src/client/ui/SendHelpMsgActivity.java
@@ -1,14 +1,29 @@
 package client.ui;
 
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import android.app.Activity;
+import android.content.res.Resources.NotFoundException;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.Window;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
+
+import communicate.PushSender;
 
 
 public class SendHelpMsgActivity extends Activity{
@@ -16,8 +31,9 @@ public class SendHelpMsgActivity extends Activity{
 	private static final String[] sen={"常用语句", "着火啦","有小偷啊","见鬼啦","被强奸啦","肚子饿了"};
 	private EditText edt;
 	private Spinner spinner;
+	private Button sendmessage;
 	private ArrayAdapter<String> adapter;
-
+    private Send send;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
@@ -27,6 +43,7 @@ public class SendHelpMsgActivity extends Activity{
 		
 		edt = (EditText) findViewById(R.id.send_msg);
 		spinner = (Spinner) findViewById(R.id.common_sentence);
+		sendmessage=(Button)findViewById(R.id.sendmessage);
 		adapter = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item, sen);
 
 		//设置下拉列表的风格
@@ -37,8 +54,17 @@ public class SendHelpMsgActivity extends Activity{
 	    
 	    //添加事件Spinner事件监听 
 	    spinner.setOnItemSelectedListener(new SpinnerSelectedListener());
-
-
+	    
+	    sendmessage.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+			    //send=new Send();
+			    //send.execute();
+				
+			}
+		});
 	}
 	
 	//使用数组形式操作
@@ -56,6 +82,26 @@ public class SendHelpMsgActivity extends Activity{
 
 	    }
 	}
+	
+	private class Send extends AsyncTask<Void, Void, String> {
 
+        @Override
+        protected String doInBackground(Void... params) { 
+        	Map<String,Object> a=new HashMap<String, Object>();
+			a.put("audio", new File("/测试/audio.amr"));
+			a.put("video", new File("/测试/video.3gp"));
+			
+            return PushSender.sendMessage("login", a);
+        }
+        @Override
+        protected void onPreExecute() {   
+        	
+        }
+        @Override
+        protected void onPostExecute(String result) {   	
+        	
+            super.onPostExecute(result);
+        }
+    }
 
 }
