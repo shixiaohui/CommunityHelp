1702c0360edd48f3059c3b8ac8d32b306429337c
wuzihua <371219521@qq.com>
2014-07-17 09:26:41 +0800

    每日进度更新 7.16 吴梓华

--- 3.开发阶段/第一次迭代/android前端/Service/src/client/ui/SetupActivity.java
+++ 3.开发阶段/第一次迭代/android前端/Service/src/client/ui/SetupActivity.java
@@ -96,11 +96,5 @@ public class SetupActivity extends Activity {
 	}
 	
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
 
 }
